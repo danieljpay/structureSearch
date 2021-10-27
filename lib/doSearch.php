c314b08f1a313e2d739c4e1e52a8a257233b137e
Daniel Pérez <plupyt_dany@hotmail.com>
2021-10-27 16:59:46 -0500

    code cleanup

--- lib/doSearch.php
+++ lib/doSearch.php
@@ -1,11 +1,11 @@
 <?php
-    include("analyzerInput.php");
+    include("structureQueryEmpresarial.php");
 
     if(isset( $_GET["inputSearch"] )) {
         $input = strtoupper($_GET["inputSearch"]);
         $words = explode(" ", $input);
         
-        analyzerInput($words);
+        structureQueryEmpresarial($words);
     } else {
         echo "<p>Tus resultados se mostrarán aquí</p>";
     }
